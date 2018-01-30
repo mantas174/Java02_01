8b6532ab6e37f34a626b776822f2978283eb2940
Mantas <mantas.muzzz@gmail.com>
2018-01-30 14:34:06 +0200

    Padariau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        String vardas;
+
+        System.out.print("Iveskite savo varda: ");
+        vardas = in.nextLine();
+
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
 
     }
 }
